f78320669699cd86cf4c1547517eb4a520624de8
gisieli <gisielioliveira@ciss.com.br>
2016-03-15 20:51:28 -0300

    Aula 15/03

--- js/controller/formCtrl.js
+++ js/controller/formCtrl.js
@@ -1,10 +1,69 @@
 myApp.controller('FormCtrl', function($scope){
 
+	$scope.indexTarefa = 0;
+	$scope.clicouEditar = false;
+
 	$scope.pessoa = {
 		name: "Gisiéli", 
 		lastname: "Oliveira",
 		age: 22,
 		phonenumber: 4699732713,
 		email:'gisielioliveira@ciss.com.br'
+	};
+
+	$scope.list = [
+	{
+		nomeTarefa: "Tarefa teste",
+		dataTarefa: '2016-03-15T05:00:58.149Z'
+	},
+	{
+		nomeTarefa: "Limpar a casa",
+		dataTarefa: '2016-03-15T05:00:58.149Z'
+	},
+	{
+		nomeTarefa: "Lavar a louça",
+		dataTarefa: '2016-03-15T05:00:58.149Z'
+	},
+	{
+		nomeTarefa: "Lavar a casa",
+		dataTarefa: '2016-03-15T05:00:58.149Z'
+	},
+	{
+		nomeTarefa: "Trabalhar - teste",
+		dataTarefa: '2016-03-15T05:00:58.149Z'
+	}
+
+	];
+
+	$scope.cadastraTarefa = function( data ) {
+		if (!!data) {
+			$scope.list.push({
+				nomeTarefa: data,
+				dataTarefa: '2016-03-15T05:00:58.149Z'
+			});
+
+			$scope.tarefa = undefined;
+		}
+	};
+
+	$scope.editaTarefa = function( dataIndex ){
+		$scope.clicouEditar = true;
+		$scope.indexTarefa = dataIndex;
+		$scope.renomear = $scope.list[dataIndex].nomeTarefa;
+	};
+
+	$scope.renomearTarefa = function (novoNome){
+		var editObj= {};
+		editObj = {
+			'nomeTarefa' : novoNome,
+			'dataTarefa' : $scope.list[$scope.indexTarefa].dataTarefa
+
+
+		}
+
+		$scope.list[$scope.indexTarefa] = editObj;
+		//$scope.indexTarefa = undefined;
+		//$scope.renomear =  undefined;
+		$scope.clicouEditar = false;
 	}
 });
