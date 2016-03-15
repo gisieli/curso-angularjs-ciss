6aad1244435b07771f3adde7dd38ce98e0c87890
gisieli <gisielioliveira@ciss.com.br>
2016-03-15 18:24:35 -0300

    Segundo commit

---
+++ js/controller/formCtrl.js
@@ -0,0 +1,10 @@
+myApp.controller('FormCtrl', function($scope){
+
+	$scope.pessoa = {
+		name: "Gisiéli", 
+		lastname: "Oliveira",
+		age: 22,
+		phonenumber: 4699732713,
+		email:'gisielioliveira@ciss.com.br'
+	}
+});
